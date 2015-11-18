7e5101e90cfd48bbf3606afcea4d4f8ac7d32a86
Vildar Khisyametdinov <vildar82@gmail.com>
2015-11-18 20:44:16 +0300

    1

---
+++ ProjectsStructure/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ProjectsStructure
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Обязательная переменная конструктора.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Освободить все используемые ресурсы.
+      /// </summary>
+      /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Код, автоматически созданный конструктором форм Windows
+
+      /// <summary>
+      /// Требуемый метод для поддержки конструктора — не изменяйте 
+      /// содержимое этого метода с помощью редактора кода.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.label1 = new System.Windows.Forms.Label();
+         this.buttonBrowse = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // textBox1
+         // 
+         this.textBox1.Location = new System.Drawing.Point(12, 22);
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size(489, 20);
+         this.textBox1.TabIndex = 0;
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(12, 6);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(117, 13);
+         this.label1.TabIndex = 1;
+         this.label1.Text = "Excel файл структуры";
+         // 
+         // buttonBrowse
+         // 
+         this.buttonBrowse.Location = new System.Drawing.Point(507, 20);
+         this.buttonBrowse.Name = "buttonBrowse";
+         this.buttonBrowse.Size = new System.Drawing.Size(27, 23);
+         this.buttonBrowse.TabIndex = 2;
+         this.buttonBrowse.Text = "...";
+         this.buttonBrowse.UseVisualStyleBackColor = true;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(607, 320);
+         this.Controls.Add(this.buttonBrowse);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.textBox1);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.TextBox textBox1;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Button buttonBrowse;
+   }
+}
+
---
+++ ProjectsStructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ProjectsStructure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectsStructure")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9be7d893-f9bc-4c08-8c2f-a3531612e5cb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
